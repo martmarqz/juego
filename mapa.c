7d208c583e96aa1732d8ec03b911f0710a7cda71
martmarqz <martmarq@umag.cl>
2026-06-25 14:19:59 -0300

    avance 7

--- mapa.c
+++ mapa.c
@@ -8,6 +8,7 @@ const int TAMAÑO_BLOQUE = 40;
 
 int mapa[15][20];
 
+//Funcion para cargar el mapa
 bool cargar_mapa(const char *nombre_archivo) {
     FILE *archivo = fopen(nombre_archivo, "r");
     
@@ -27,11 +28,14 @@ bool cargar_mapa(const char *nombre_archivo) {
     return true;
 }
 
+//Verifica si el personaje se estrella con una pared
 bool hay_colision(float x, float y) {
     int borde_izquierdo = x / TAMAÑO_BLOQUE;
     int borde_derecho = (x + 30) / TAMAÑO_BLOQUE;
     int borde_superior = y / TAMAÑO_BLOQUE;
     int borde_inferior = (y + 30) / TAMAÑO_BLOQUE;
+
+    //Verifica si choca con los bordes
 
     if (mapa[borde_superior][borde_izquierdo] == 1) return true;
     if (mapa[borde_superior][borde_derecho] == 1) return true;
@@ -43,22 +47,29 @@ bool hay_colision(float x, float y) {
 
 int main() 
 {
+    //llama a la funcion mapa, intenta leerla, al no poder aborta mision
     if (!cargar_mapa("nivel1.txt")) {
         return -1; 
     }
 
+    //inicia allegro
     if (!al_init()) return -1;
+
+    //inicia el teclado y el dibujo
     al_install_keyboard();
     al_init_primitives_addon();
 
+    //crea la pantalla, la cola de eventos y el reloj
     ALLEGRO_DISPLAY *pantalla = al_create_display(800, 600);
     ALLEGRO_EVENT_QUEUE *cola_eventos = al_create_event_queue();
     ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
 
+    //registra los eventos, el timer y la pantalla
     al_register_event_source(cola_eventos, al_get_keyboard_event_source());
     al_register_event_source(cola_eventos, al_get_display_event_source(pantalla));
     al_register_event_source(cola_eventos, al_get_timer_event_source(timer));
 
+    //interruptor para hacer correr el juegoo y dibujar
     bool corriendo = true;
     bool dibujar = true;
 
@@ -68,38 +79,53 @@ int main()
     float gravedad = 0.5;      
     float fuerza_salto = -9.0; 
     
+    // interrumptor teclas del teclado
     bool tecla_izquierda = false;
     bool tecla_derecha = false;
 
+    //inicia el reloj
     al_start_timer(timer);
 
+    //El bucle donde todo pasa
     while (corriendo) {
+        //Se crea el sobre de eventos
         ALLEGRO_EVENT evento;
+        //pone en pausa el ciclo
         al_wait_for_event(cola_eventos, &evento);
 
+        //cuando se cierra la ventana el interruptor corriendo se apaga
         if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             corriendo = false;
         }
+
+        //maneja el teclado
         else if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
+
+            //el switch sirve para cada caso
             switch (evento.keyboard.keycode) {
+                //Activa las flechas izquierda y derecha
                 case ALLEGRO_KEY_LEFT: tecla_izquierda = true; break;
                 case ALLEGRO_KEY_RIGHT: tecla_derecha = true; break;
+                //evita que al saltar, sea un salto infinito
                 case ALLEGRO_KEY_UP: 
                     if (hay_colision(jugador_x, jugador_y + 1)) {
                         velocidad_y = fuerza_salto; 
                     }
                     break;
+                //Cierra el juego
                 case ALLEGRO_KEY_ESCAPE: corriendo = false; break;
             }
         }
+        //Cuando se levanta el dedo del teclado se activa esto
         else if (evento.type == ALLEGRO_EVENT_KEY_UP) {
             switch (evento.keyboard.keycode) {
                 case ALLEGRO_KEY_LEFT: tecla_izquierda = false; break;
                 case ALLEGRO_KEY_RIGHT: tecla_derecha = false; break;
             }
         }
-
+        //motor y corazon del juego
         else if (evento.type == ALLEGRO_EVENT_TIMER) {
+            //Movimiento horizontal
             if (tecla_izquierda) {
                 jugador_x -= 4.0;
                 if (hay_colision(jugador_x, jugador_y)) jugador_x += 4.0;
@@ -109,9 +135,11 @@ int main()
                 if (hay_colision(jugador_x, jugador_y)) jugador_x -= 4.0;
             }
 
+            //caida libre
             velocidad_y += gravedad; 
             jugador_y += velocidad_y;
 
+            //colisiones verticales
             if (hay_colision(jugador_x, jugador_y)) {
                 if (velocidad_y > 0) {
                     jugador_y = ((int)(jugador_y + 30) / TAMAÑO_BLOQUE) * TAMAÑO_BLOQUE - 30.1;
@@ -122,29 +150,35 @@ int main()
                 velocidad_y = 0; 
             }
 
+            //actualiza los graficos
             dibujar = true;
         }
 
+        //Solo dibuja si hay algun cambio
         if (dibujar && al_is_event_queue_empty(cola_eventos)) {
             dibujar = false;
+
+            //Limpia el mapa
             al_clear_to_color(al_map_rgb(20, 30, 50)); 
 
-           
+            //Pinta el mapa
             for (int fila = 0; fila < 15; fila++) {
                 for (int columna = 0; columna < 20; columna++) {
                     if (mapa[fila][columna] == 1) {
                         float x_bloque = columna * TAMAÑO_BLOQUE;
                         float y_bloque = fila * TAMAÑO_BLOQUE;
-                        
+                        //Relleno
                         al_draw_filled_rectangle(x_bloque, y_bloque, x_bloque + TAMAÑO_BLOQUE, y_bloque + TAMAÑO_BLOQUE, al_map_rgb(100, 100, 110));
+                        //borde
                         al_draw_rectangle(x_bloque, y_bloque, x_bloque + TAMAÑO_BLOQUE, y_bloque + TAMAÑO_BLOQUE, al_map_rgb(50, 50, 60), 1);
                     }
                 }
             }
 
-            // DIBUJAR AL JUGADOR
+            // Dibuja al jugador
             al_draw_filled_rectangle(jugador_x, jugador_y, jugador_x + 30, jugador_y + 30, al_map_rgb(255, 60, 60));
 
+            //Crea un lienzo en 2do plano 
             al_flip_display();
         }
     }
--- proyecto.c
+++ proyecto.c
@@ -8,6 +8,9 @@
 #include<allegro5/allegro_primitives.h> //Dibujo 
 #include<allegro5/allegro_audio.h> //audio
 #include<allegro5/allegro_acodec.h> //audio codecs
+#define TAM_TILE 40
+#define fila 15
+#define columna 20
 
 struct arma_
 {
@@ -22,6 +25,7 @@ struct personaje_
 {
     float x;
     float y;
+    float velocidad;
     int vida;
     int energia;
     bool ataque;
@@ -37,7 +41,7 @@ struct enemigo_
     int energia;
     int daño;
     int alcance;
-    int velocidad;
+    float velocidad;
     bool ataque;
 };
 typedef struct enemigo_ enemigo;
@@ -60,28 +64,175 @@ struct pocion_
 };
 typedef struct pocion_ pocion;
 
+int mapa[fila][columna];
+personaje heroe;
+personaje amigo;
+enemigo guardia;
+enemigo jefe;
+enemigo basico;
+cofre cofrecito;
+
 void limites_pantalla(float *x, float *y);
-void gravedad(float *y, float *velocidad_caida);
-bool impacto(float x1, float y1, float x2, float y2);
-void cargar_mapa(const char *nombre_archivo);
+bool colision(float x,float y);
+bool cargar_mapa(const char *nombre_archivo);
 
 int main()
 {
+    if(!cargar_mapa("nivel1.txt"))
+    {
+        return -1;
+    }
+
     if(!al_init()) 
     {
         printf("Error al abrir Allegro");
         return -1;
     }
 
-    personaje persona;
     al_init_primitives_addon();
-    al_install_keyboard(); //teclado
+    al_install_keyboard(); 
+    al_init_image_addon();
+
     ALLEGRO_DISPLAY *display = al_create_display(800, 600);
     ALLEGRO_TIMER *timer = al_create_timer(1.0 / 60.0);
     ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
+    ALLEGRO_BITMAP *fondo_nivel1=al_load_bitmap("Fondo1.jpg");
+    if(!fondo_nivel1)
+    {
+        printf("Error fatal: No se encontro 'Fondo1.jpg'\n");
+        return -1;
+    }
+
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool corriendo=true;
+    bool dibujar=true;
+    bool izquierda=false;
+    bool derecha=false;
+    int i,j;
+    float x_bloque,y_bloque,velocidad_caida=0.0;
+
+    al_start_timer(timer);
+
+    while (corriendo)
+    {
+        ALLEGRO_EVENT evento;
+        al_wait_for_event(queue, &evento);
+
+        if(evento.type==ALLEGRO_EVENT_DISPLAY_CLOSE)
+        {
+            corriendo=false;
+        }
+        else if(evento.type==ALLEGRO_EVENT_KEY_DOWN)
+        {
+            switch (evento.keyboard.keycode)
+            {
+            case ALLEGRO_KEY_LEFT:
+                izquierda=true;
+                break;
+            
+            case ALLEGRO_KEY_RIGHT:
+                derecha=true;
+                break;
+
+            case ALLEGRO_KEY_UP:
+                if(colision(heroe.x,heroe.y+1.0))
+                {
+                    velocidad_caida=-9.0;
+                }
+                break;
+            case ALLEGRO_KEY_ESCAPE:
+                corriendo=false;
+                break;
+            }
+        }
+        else if(evento.type==ALLEGRO_EVENT_KEY_UP)
+        {
+            switch (evento.keyboard.keycode)
+            {
+            case ALLEGRO_KEY_LEFT:
+                izquierda=false;
+                break;
+            
+            case ALLEGRO_KEY_RIGHT:
+                derecha=false;
+                break;
+            }
+        }
+        else if(evento.type==ALLEGRO_EVENT_TIMER)
+        {
+            if(izquierda)
+            {
+                heroe.x-=4.0;
+                if(colision(heroe.x,heroe.y))
+                {
+                    heroe.x+=4.0;
+                }
+            }
+            if(derecha)
+            {
+                heroe.x+=4.0;
+                if(colision(heroe.x,heroe.y))
+                {
+                    heroe.x-=4.0;
+                }
+            }
+            velocidad_caida+=0.5;
+            heroe.y+=velocidad_caida;
+            if(colision(heroe.x,heroe.y))
+            {
+                if(velocidad_caida>0)
+                {
+                    heroe.y = ((int)(heroe.y + 30) / TAM_TILE) * TAM_TILE - 30.1;
+                }
+                else if (velocidad_caida < 0) 
+                {
+                    heroe.y = ((int)heroe.y / TAM_TILE) * TAM_TILE + TAM_TILE + 0.1;
+                }
+                velocidad_caida = 0;
+            }
+            dibujar=true;
+        }
+
+        if(dibujar&&al_is_event_queue_empty(queue))
+        {
+            dibujar=false;
+            al_clear_to_color(al_map_rgb(20,30,50));
+
+            al_draw_scaled_bitmap(
+                fondo_nivel1,
+                0,0,al_get_bitmap_width(fondo_nivel1),al_get_bitmap_height(fondo_nivel1),
+                0,0,columna*TAM_TILE,fila*TAM_TILE,
+                0
+            );
+
+            for(i=0;i<fila;i++)
+            {
+                for(j=0;j<columna;j++)
+                {
+                    if(mapa[i][j]==1)
+                    {
+                        x_bloque=j*TAM_TILE;
+                        y_bloque=i*TAM_TILE; 
+                        al_draw_filled_rectangle(x_bloque,y_bloque,x_bloque+TAM_TILE,y_bloque+TAM_TILE,al_map_rgb(100,100,100));
+                        al_draw_rectangle(x_bloque,y_bloque,x_bloque+TAM_TILE,y_bloque+TAM_TILE,al_map_rgb(50,50,60),1);
+                    }
+                }
+            }
+            al_draw_filled_rectangle(heroe.x,heroe.y,heroe.x+30,heroe.y+30,al_map_rgb(255,50,50));
+
+            al_flip_display();
+        }
+    }
+    
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+    al_destroy_display(display);
+    al_destroy_bitmap(fondo_nivel1);
+    
+    return 0;
 }
 
 void limites_pantalla(float *x, float *y)
@@ -104,78 +255,88 @@ void limites_pantalla(float *x, float *y)
     }
 }
 
-void gravedad(float *y, float *velocidad_caida)
+bool colision(float x,float y)
 {
-    *y+=*velocidad_caida;
-    if(*y>500)
+    int izq=x/TAM_TILE;
+    int der=(x+30)/TAM_TILE;
+    int arr=y/TAM_TILE;
+    int aba=(y+30)/TAM_TILE;
+    if(izq<0||der>=20||arr<0||aba>=15)
     {
-        *y=500;
-        *velocidad_caida=0;
+        return true;
     }
-}
-
-bool impacto(float x1, float y1, float x2, float y2)
-{
-    if(x1<x2+50 && x1+50>x2 && y1<y2+50 && y1+50>y2)
+    if (mapa[arr][izq] == 1) 
+    {
+        return true;
+    }
+    if (mapa[arr][der] == 1) 
+    {
+        return true;
+    }
+    if (mapa[aba][izq] == 1) 
+    {
+        return true;
+    }
+    if (mapa[aba][der] == 1) 
     {
         return true;
     }
     return false;
 }
 
-void cargar_mapa(const char *nombre_archivo, personaje p)
+bool cargar_mapa(const char *nombre_archivo)
 {
     int i,j;
     FILE *archivo=fopen(nombre_archivo, "r");
     if(archivo==NULL)
     {
         printf("Error al abrir el archivo");
-        return;
+        return false;
     }
-    int mapa[12][16];
-    for (i=0;i<12;i++)
+    for (i=0;i<fila;i++)
     {
-        for(j=0;j<16;j++)
+        for(j=0;j<columna;j++)
         {
             fscanf(archivo,"%d",&mapa[i][j]);
             if(mapa[i][j]==9)
             {
-                p.x=TAM_TILE*j;
-                p.y=TAM_TILE*i;  
+                heroe.x=TAM_TILE*j;
+                heroe.y=TAM_TILE*i;  
                 mapa[i][j]=0;
             }
             else if(mapa[i][j]==8)
             {
-                alien_guardia.x=TAM_TILE*j;
-                alien_guardia.y=TAM_TILE*i;
+                guardia.x=TAM_TILE*j;
+                guardia.y=TAM_TILE*i;
                 mapa[i][j]=0;
             }
             else if(mapa[i][j]==7)
             {
-                alien_jefe.x=TAM_TILE*j;
-                alien_jefe.y=TAM_TILE*i;
+                jefe.x=TAM_TILE*j;
+                jefe.y=TAM_TILE*i;
                 mapa[i][j]=0;
             }
             else if(mapa[i][j]==6)
             {
-                compañero.x=TAM_TILE*j;
-                compañero.y=TAM_TILE*i;
+                amigo.x=TAM_TILE*j;
+                amigo.y=TAM_TILE*i;
                 mapa[i][j]=0;
             }
             else if(mapa[i][j]==5)
             {
-                alien_enemigo.x=TAM_TILE*j;
-                alien_enemigo.y=TAM_TILE*i;
+                basico.x=TAM_TILE*j;
+                basico.y=TAM_TILE*i;
                 mapa[i][j]=0;
             }
             else if(mapa[i][j]==4)
             {
-                cofre.x=TAM_TILE*j;
-                cofre.y=TAM_TILE*i;
+                cofrecito.x=TAM_TILE*j;
+                cofrecito.y=TAM_TILE*i;
                 mapa[i][j]=0;
             }
+            
         }
     }
     fclose(archivo);
-    return;
+    return true;
 }
